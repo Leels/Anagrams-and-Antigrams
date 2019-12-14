57c44997d5a323df67383eb21302fc437afb1175
Lela Smith <smith.lelaa@gmail.com>
2019-12-13 20:35:25 -0800

    refactor code

--- lib/anagrams.rb
+++ lib/anagrams.rb
@@ -9,26 +9,20 @@ class Anagrams_Antigrams
   def not_word
     if !input1.downcase.chars.any? { |i| ["a", "e", "i", "o", "u", "y"].include? i } || !input2.downcase.chars.any? { |i| ["a", "e", "i", "o", "u", "y"].include? i }
       true
-    else
-      false
     end
   end
 
   def antigram
     if !input1.downcase.chars.any? { |i| (input2.downcase.chars).include? i }
       true
-    else
-      false
     end
   end
 
   def anagram
-    word1 = ((@input1.downcase.chars.sort.join).tr(" ", "")).tr("\'", "")
-    word2 = ((@input2.downcase.chars.sort.join).tr(" ", "")).tr("\'", "")
+    word1 = ((input1.downcase.chars.sort.join).tr(" ", "")).tr("\'", "")
+    word2 = ((input2.downcase.chars.sort.join).tr(" ", "")).tr("\'", "")
     if word1 == word2
       true
-    else
-      false
     end
   end
 end
