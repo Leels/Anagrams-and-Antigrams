f600cc7510d56e79a38e61136396accb6625c7a2
Lela <smith.lelaa@gmail.com>
2019-12-16 20:31:53 -0800

    add tests to check all methods

--- spec/anagrams_spec.rb
+++ spec/anagrams_spec.rb
@@ -4,40 +4,25 @@ require ('anagrams')
 
 describe('anagram') do
 
-  # it('will take in a string and convert it into an array') do
-  # anagrams_antigrams =  Anagrams_Antigrams.new('cat')
-  #   expect(anagrams_antigrams.anagram()).to(eq(['c','a','t']))
-  # end
-  # it('will alphabetize array and return to string') do
-  # anagrams_antigrams =  Anagrams_Antigrams.new('cat')
-  #   expect(anagrams_antigrams.anagram()).to(eq('act'))
-  # end
-  # it('will compare two words to see if they are anagrams') do
-  #   anagrams_antigrams =  Anagrams_Antigrams.nsew('cat', 'act')
-  #   expect(anagrams_antigrams.anagram()).to(eq(true))
-  # end
-  # it('will ccount for words that have different cases') do
-  #   anagrams_antigrams =  Anagrams_Antigrams.new('Eat', 'Tea')
-  #   expect(anagrams_antigrams.anagram()).to(eq(true))
-  # end
-  # it('will ccount for words that have different cases') do
-  #   anagrams_antigrams =  Anagrams_Antigrams.new('young lad', 'an old man')
-  #   expect(anagrams_antigrams.anagram()).to(eq(true))
-  # end
-  # it('will caccount for words that have apostrophes') do
-  #   anagrams_antigrams =  Anagrams_Antigrams.new('don\'t', 'dont')
-  #   expect(anagrams_antigrams.anagram()).to(eq(true))
-  # end
-  # it('will caccount for words that have apostrophes') do
-  #   anagrams_antigrams =  Anagrams_Antigrams.new('don\'t', 'dont')
-  #   expect(anagrams_antigrams.anagram()).to(eq(true))
-  # end
-  # it('will ask user to input an actual word if there are no vowels') do
-  #   anagrams_antigrams =  Anagrams_Antigrams.new('hpg', 'dtt')
-  #   expect(anagrams_antigrams.anagram()).to(eq("Please enter an actual word"))
-  # end
-  it('will check if the two words are antigrams') do
-    anagrams_antigrams =  Anagrams_Antigrams.new('an old guy', 'young lad')
+
+  it('will compare two words to see if they are anagrams') do
+    anagrams_antigrams =  Anagrams_Antigrams.new('cat', 'act')
     expect(anagrams_antigrams.anagram()).to(eq(true))
+  end
+  it('will account for words with different cases') do
+    anagrams_antigrams =  Anagrams_Antigrams.new('Bury', 'Ruby')
+    expect(anagrams_antigrams.anagram()).to(eq(true))
+  end
+  it('It will account for multiple words being submitted') do
+    anagrams_antigrams =  Anagrams_Antigrams.new('Young lad', 'An old guy')
+    expect(anagrams_antigrams.anagram()).to(eq(true))
+  end
+  it('will ask user to input an actual word if there are no vowels') do
+    anagrams_antigrams =  Anagrams_Antigrams.new('hpg', 'dtt')
+    expect(anagrams_antigrams.not_word()).to(eq(true))
+  end
+  it('will check if the two words are antigrams') do
+    anagrams_antigrams =  Anagrams_Antigrams.new('pretty', 'moon')
+    expect(anagrams_antigrams.antigram()).to(eq(true))
   end
 end
